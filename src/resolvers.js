3c6f4ab1679cf114c7819507d953ba359de00586
rmartinezm <robertomtz@ciencias.unam.mx>
2018-02-18 00:30:23 -0600

    Finished: Simple CRUD

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,18 +1,87 @@
 export default {
     Query: {
         allUsers: async (parent, args, { User }) => {
-            let users = await User.find();
-            return users.map(user => {
-                user._id = user._id.toString();
-                return user;
-            });
+            return await User.find();
+        },
+        getUser: async (parent, { id }, { User }) => {
+            return await User.findOne({'_id': id})
+                /* USER NOT FOUND*/
+                .catch(err => null);
         }
     },
     Mutation: {
         createUser: async (parent, args, { User }) => {
-            let user = await new User(args).save();
-            user._id = user._id.toString();
-            return user;
+            return await new User(args).save();
+        },
+        removeUser: async (parent, { id }, { User }) => {
+            let user = await User.findOne({'_id': id})
+                /* USER NOT FOUND*/
+                .catch(err => null);
+            return user == null? null: await user.remove();
+        },
+        updateEmal: async (parent, { id, email }, { User }) => {
+            let user = await User.findOne({'_id': id})
+                /* USER NOT FOUND*/
+                .catch(err => null);
+            if (user == null)
+                return null;
+            user.email = email;
+            return await user.save();
+        },
+        updatePassword: async (parent, { id, password }, { User }) => {
+            let user = await User.findOne({'_id': id})
+                /* USER NOT FOUND*/
+                .catch(err => null);
+            if (user == null)
+                return null;
+            user.password = password;
+            return await user.save();
+        },
+        updateUsername: async (parent, { id, username }, { User }) => {
+            let user = await User.findOne({'_id': id})
+                /* USER NOT FOUND*/
+                .catch(err => null);
+            if (user == null)
+                return null;
+            user.username = username;
+            return await user.save();
+        },
+        addCourseToThisUser: async (parent, { id, course }, { User }) => {
+            let user = await User.findOne({'_id': id})
+                /* USER NOT FOUND*/
+                .catch(err => null);
+            if (user == null)
+                return null;
+            let flag = true;
+            user.courses.forEach( eachCourse => {
+                if(eachCourse == course) flag = false;
+            });
+            if (flag)
+                user.courses.push(course);
+            return await user.save();
+        },
+        removeCourseToThisUser: async (parent, { id, course }, { User }) => {
+            let user = await User.findOne({'_id': id})
+                /* USER NOT FOUND*/
+                .catch(err => null);
+            if (user == null)
+                return null;
+            let flag = false;
+            user.courses.forEach( eachCourse => {
+                if(eachCourse == course) flag = true;
+            });
+            if (flag)
+                user.courses.splice(user.courses.indexOf(course), 1);
+            return await user.save();
+        },
+        clearCoursesToThisUser: async (parent, { id }, { User }) => {
+            let user = await User.findOne({'_id': id})
+                /* USER NOT FOUND*/
+                .catch(err => null);
+            if (user == null)
+                return null;
+            user.courses.splice(0);
+            return await user.save();
         }
     }
 }
--- src/schema.js
+++ src/schema.js
@@ -1,19 +1,25 @@
 export default `
-
     type User {
         _id: String!
         email: String!
         password: String!
-        username: String
-        courses: [String]
+        username: String!
+        courses: [String]!
     }
 
     type Query {
         allUsers: [User!]!
+        getUser(id: String!): User
     }
 
     type Mutation {
-        createUser(email: String!, password: String!) : User!
+        createUser(email: String!, password: String!, username: String!) : User!
+        removeUser(id: String!): User
+        updateEmal(id: String!, email: String!): User
+        updatePassword(id: String!, password: String!): User
+        updateUsername(id: String!, username: String!): User
+        addCourseToThisUser(id: String!, course: String!): User
+        removeCourseToThisUser(id: String!, course: String!): User
+        clearCoursesToThisUser(id: String!): User
     }
-
 `;
