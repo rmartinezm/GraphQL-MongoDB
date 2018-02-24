aa9d9a5ac9ac9323346d9673e4f2e383177af597
rmartinezm <robertomtz@ciencias.unam.mx>
2018-02-23 19:12:07 -0600

    Added: Simple query from ReactJS with Apollo

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// Apollo imports
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const query = gql`
+query {
+  allUsers {
+    username
+    courses
+  }
+}
+`;
+
+const client = new ApolloClient({
+    link: new HttpLink({ uri: 'http://localhost:3300/graphql' }),
+    cache: new InMemoryCache()
+});
+
+client.query({
+    query
+}).then(res => console.log(res.data));
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+    , document.getElementById('root'));
 registerServiceWorker();
