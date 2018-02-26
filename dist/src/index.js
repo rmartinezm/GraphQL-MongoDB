402477c4682c52849a27e316e341763f753c9827
rmartinezm <robertomtz@ciencias.unam.mx>
2018-02-26 10:33:46 -0600

    Changed: Apollo version

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,18 +4,18 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 // Apollo imports
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
+import ApolloClient from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo';
 
-const apolloClient = new ApolloClient({
+const client = new ApolloClient({
     link: new HttpLink({ uri: 'http://localhost:3300/graphql' }),
-    cache: new InMemoryCache()
+    cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
 });
 
 ReactDOM.render(
-    <ApolloProvider client={apolloClient}>
+    <ApolloProvider client={client}>
         <App />
     </ApolloProvider>
     , document.getElementById('root'));
